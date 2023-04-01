df2e4cfab0f8e99cd50722a769b24c4bba475053
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-04-01 20:02:46 +0300

    added PlatformRepo

---
+++ Data/PlatfromRepo.cs
@@ -0,0 +1,45 @@
+﻿using PlatformService.Data.Repos;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatfromRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+        public PlatfromRepo(AppDbContext context)
+        {
+            _context = context; 
+        }
+        public void CreateaPlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _context.Platforms.Add(platform);
+            _context.SaveChanges();
+            
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            var res = _context.Platforms.FirstOrDefault(p => p.Id == id); 
+            if (res == null)
+            {
+                throw new NullReferenceException($"{nameof(res)} doesnt' exist ");
+            }
+            return res;
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ Data/Repos/IPlatformRepo.cs
@@ -0,0 +1,15 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data.Repos
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreateaPlatform(Platform platform);
+    }
+}
