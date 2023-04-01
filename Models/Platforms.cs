0b98b7688865a1585fa6c60b9f94caf9730ccbb7
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-04-01 19:39:16 +0300

    added platform model

---
+++ Models/Platforms.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platforms
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string Cost { get; set; }
+    }
+}
