7daec1a3fc4eb5cf337269effcd0000f08527342
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-04-02 11:06:23 +0300

    added platfrom API contrller and some actions

---
+++ Controllers/PlatfromsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data.Repos;
+using PlatformService.DTOs;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatfromsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+
+        public PlatfromsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatfromReadDTO>> GetPlatfroms()
+        {
+            var platfromItems = _platformRepo.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatfromReadDTO>>(platfromItems));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PlatfromReadDTO> GetplafromById(int id)
+        {
+            var platfrom = _platformRepo.GetPlatformById(id);
+
+            if(platfrom == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<PlatfromReadDTO>(platfrom));
+        }
+
+    }
+}
