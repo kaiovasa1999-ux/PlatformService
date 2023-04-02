b4e86baa9f72b02180a47932bd1cc08e4c24f649
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-04-02 10:46:00 +0300

    createa mappings

---
+++ Profiles/PlatfromProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatfromProfile : Profile
+    {
+        public PlatfromProfile()
+        {
+            //from => to
+            //source => target
+
+            CreateMap<Platform, PlatfromReadDTO>();
+            CreateMap<PlatfromCreateDTO, Platform>();
+
+        }
+    }
+}
