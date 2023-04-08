fbae06b7396c43a58accccd39dd9b8e61523cc4f
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-04-08 13:21:08 +0300

    implement just one end poin which PlatformService will call

---
+++ CommandsService/Controllers/CommandsServiceController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    public class CommandsServiceController : ControllerBase
+    {
+    }
+}
--- CommandsService/Controllers/PlatformServiceController.cs
+++ CommandsService/Controllers/PlatformServiceController.cs
@@ -3,7 +3,19 @@
 namespace CommandsService.Controllers
 {
     [ApiController]
+    [Route("api/c/[controller]")]
     public class PlatformServiceController : ControllerBase
     {
+        public PlatformServiceController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestConnection()
+        {
+            Console.WriteLine("connecting is done!");
+            return Ok("Was Okey!");
+        }
     }
 }
